597d97e99f21796de0418ecd7d88c296dd9b2c7c
Kamil <babaevkamil@gmail.com>
2021-05-27 11:23:55 +0300

    Create utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+export default function isEmptyArray(value: unknown): boolean {
+  if (typeof value === 'string' {
+    return value === '[]'
+  }
+
+  if (Array.isArray(value) && value.length === 0) {
+    return true;
+  }
+
+  return false;
+}
